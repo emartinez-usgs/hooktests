2b62544710e3579578940c87974a92263ebc70bc
Eric Martinez <emartinez@usgs.gov>
2015-04-22 11:11:06 -0700

    Added documentation.

--- scripts/post-commit.sh
+++ scripts/post-commit.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+##
+# This script is meant to be run as a post-commit hook on simple projects where
+# auto-deployment to a subsequent server is desirable.
+#
+# Specifically post-commit, it pushes the master branch to the origin remote
+# then logs in to a configured set of deployHosts over SSH and pulls the
+# master branch down from the origin remote.
+#
+# NB: If the current branch is not the master branch, nothing is done. This
+#     restriction may be removed in the future, but for simplicity, this
+#     script currently only works on the master bracn with the origin remote.
+#
+# Requirements:
+#   (1) Passwordless origin remote for pushes on local system
+#   (2) Passwordless SSH to each server configured in deployHosts
+#   (3) Passwordless origin remote for pulls on each server configured
+#       in deployHosts
+##
+
+
+## Configuration
+
 #
 # config file provides:
 #  - deployPath
@@ -8,6 +30,8 @@
 #
 source ".git/hooks/config.sh";
 
+
+## Usage
 if [[ -z "$deployPath" || -z "$deployHosts" || -z "$deployUser" ]]; then
   echo "Incomplete configuration file. Check config.sh.";
   exit -1;
@@ -21,6 +45,9 @@ if [ "$currentBranch" != "master" ]; then
   exit 0;
 fi
 
+
+## Script
+
 git push $remote master;
 
 command="cd $deployPath";
@@ -31,5 +58,7 @@ for server in $deployHosts; do
   ssh -l $deployUser $server "($command)";
 done
 
+
+## Done
 
 exit 0;
